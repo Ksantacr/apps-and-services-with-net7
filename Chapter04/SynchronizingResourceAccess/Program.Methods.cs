962bac8a25d64b6d975237b455b763e0e82bee08
Kevin Santacruz <defonix0@gmail.com>
2024-02-20 15:09:26 -0500

    feat(benchmarking): :tada: Examples for benchmarking performance, multitasking and concurrency

---
+++ Chapter04/SynchronizingResourceAccess/Program.Methods.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+
+partial class Program
+{
+
+    // Best practices: https://learn.microsoft.com/en-us/dotnet/standard/threading/managed-threading-best-practices
+    // Field like events: https://learn.microsoft.com/en-us/archive/blogs/cburrows/field-like-events-considered-harmful
+    // Threadsafe events: https://blog.stephencleary.com/2009/06/threadsafe-events.html
+    static void MethodA()
+    {
+        try
+        {
+            if(Monitor.TryEnter(SharedObjects.Conch, TimeSpan.FromSeconds(15))) {
+                //lock (SharedObjects.Conch)
+                //{
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(Random.Shared.Next(2000));
+                    SharedObjects.Message += "A";
+                    Interlocked.Increment(ref SharedObjects.Counter);
+                    Write(".");
+                }
+                //}
+            } else
+            {
+                WriteLine("Method A timed out when entering a monitor on conch.");
+            }
+        }
+        finally
+        {
+            Monitor.Exit(SharedObjects.Conch);
+        }
+        
+    }
+    static void MethodB()
+    {
+        lock (SharedObjects.Conch)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(Random.Shared.Next(2000));
+                SharedObjects.Message += "B";
+                Interlocked.Increment(ref SharedObjects.Counter);
+                Write(".");
+            }
+        }
+    }
+}
---
+++ Chapter04/WorkingWithTasks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+//OutputThreadInfo();
+Stopwatch timer = Stopwatch.StartNew();
+//SectionTitle("Running methods synchronously on one thread.");
+//MethodA();
+//MethodB();
+//MethodC();
+//WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+
+//SectionTitle("Running methods asynchronously on multiple threads.");
+//timer.Restart();
+//Task taskA = new(MethodA);
+////taskA.Start();
+//Task taskB = Task.Factory.StartNew(MethodB);
+//Task taskC = Task.Run(MethodC);
+////WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+
+//Task[] tasks = { taskA, taskB, taskC };
+//Task.WaitAll(tasks);
+
+
+//SectionTitle("Passing the result of one task as an input into another.");
+//timer.Restart();
+//Task<string> taskServiceThenSProc = Task.Factory
+// .StartNew(CallWebService) // returns Task<decimal>
+// .ContinueWith(previousTask => // returns Task<string>
+// CallStoredProcedure(previousTask.Result));
+//WriteLine($"Result: {taskServiceThenSProc.Result}");
+//WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+
+SectionTitle("Nested and child tasks");
+Task outerTask = Task.Factory.StartNew(OuterMethod);
+outerTask.Wait();
+WriteLine("Console app is stopping.");
